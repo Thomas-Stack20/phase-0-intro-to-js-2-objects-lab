684ae11697c7f9d1da16b3cb94e2f24462fab26f
thomas <thomascoles14@gmail.com>
2021-03-20 15:01:10 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Thomas', 
+    streetAddress: '5034, Peanut'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee['name']='Sam';
+    newEmployee['streetAddress'] = '11 Broadway' 
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee['Sam', '11 Broadway']
+    return employee
+}
